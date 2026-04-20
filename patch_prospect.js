eaacd22a72bfadffe2dae0cca6a4db7d454ce972
La Planete Bois <laplanetebois.creator@gmail.com>
2026-04-20 11:51:07 +0200

    update dashboard via Claude Code

--- patch_prospect.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require("fs");
-let h = fs.readFileSync("index.html", "utf8");
-const start = h.indexOf("window.prospectView = function(idx) {");
-const end = h.indexOf("\nwindow._prospectToPRM", start);
-console.log("start:"+start+" end:"+end);
